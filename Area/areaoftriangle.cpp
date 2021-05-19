1625b2ddf875e0bcc2bbcb4cd14ed0ebcbc1b0d9
Suman Basnet <80586555+Basnetsuman4@users.noreply.github.com>
2021-05-19 11:38:56 +0545

    Update areaoftriangle.cpp

--- Area/areaoftriangle.cpp
+++ Area/areaoftriangle.cpp
@@ -5,10 +5,8 @@ int main()
     float base, height, area;
     cout<<"\tEnter the base of the triangle : ";
     cin>>base;
-    cout<<" unit.";
     cout<<"\t\nEnter the height of the triangle : ";
-    cin>>height;
-    cout<<" unit.";
+    cin>>height:
     area = 0.5*base*height;
     cout<<"\t\nThe area of the triangle is : "<<area<<" unit square";
     return 0;
