152332aebf4f70c571f3e6759348e56d9e7ba931
Suman Basnet <Arunbasnet54@gmail.com>
2021-05-28 21:15:06 +0545

    Check integer 2

---
+++ Assignments/Assignment_2/Check_integer_2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"\tEnter the number : ";
+    cin>>num;
+    if(num == 0)
+    {
+        cout<<"\n\tThe number is zero.";
+
+    }
+    else if(num > 0)
+    {
+        cout<<"\n\tThe number is positive.";
+    }
+    else
+    {
+        cout<<"\n\tThe number is negative.";
+    }     
+
+    return 0;
+}
