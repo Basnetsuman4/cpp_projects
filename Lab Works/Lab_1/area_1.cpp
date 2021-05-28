2092fe344d1a35c9d089a8af7163fe8c4217e220
Suman Basnet <80586555+Basnetsuman4@users.noreply.github.com>
2021-05-28 20:27:35 +0545

    Area of triangle

--- Lab Works/Lab_1/area_1.cpp
+++ Lab Works/Lab_1/area_1.cpp
@@ -5,6 +5,7 @@ int main()
     float base , height, area;
     cout<<"\tEnter the base of the triangle : ";
     cin>>base;
+    
     cout<<"\t\nEnter the height of the triangle : ";
     cin>>height;
     area = 0.5*base*height;
