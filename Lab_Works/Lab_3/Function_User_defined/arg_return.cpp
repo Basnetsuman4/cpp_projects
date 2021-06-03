1424ad1dbf7d5fec71f308d31558ff48773faec6
Suman Basnet <Arunbasnet54@gmail.com>
2021-06-03 10:28:44 +0545

    Argument with return type

---
+++ Lab_Works/Lab_3/Function_User_defined/arg_return.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h> 
+using namespace std;
+
+float get_area(float side1, float side2 , float side3);
+int main()
+{
+    float s, area , side1 , side2 , side3;
+    cout<<"\tEnter the first side of the triangle : ";
+    cin>>side1;
+    cout<<"\n\tEnter the second side of the triangle : ";
+    cin>>side2;
+    cout<<"\n\tEnter the third side of the triangle : ";
+    cin>>side3;
+    get_area(side1, side2, side3);
+    cout<<"\n\tThe area of the triangle is : "<<area<<" unit square.";
+
+}
+
+float get_area(float side1 , float side2 , float side3)
+{
+    float s , area;
+    s =((side1 + side2 +side3)/2);
+    area = sqrt(s*(s-side1)*(s-side2)*(s-side3));
+    return area;
+}
