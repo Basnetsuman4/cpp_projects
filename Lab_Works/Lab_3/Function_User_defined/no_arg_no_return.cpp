32843b4b3c0093a359562ffce08f6ac978a2416f
Suman Basnet <Arunbasnet54@gmail.com>
2021-06-03 08:57:34 +0545

    No argument no return type

--- Lab_Works/Lab_3/Function_User_defined/no_arg_no_return.cpp
+++ Lab_Works/Lab_3/Function_User_defined/no_arg_no_return.cpp
@@ -11,7 +11,7 @@ int main()
 
 void area()
 {    
-    float s, side1, side2 , side3, area;
+    float s , side1, side2 , side3, area;
     cout<<"\tEnter the first side of the triangle : ";
     cin>>side1;
     cout<<"\n\tEnter the second side of the trianlge : ";
@@ -20,5 +20,5 @@ void area()
     cin>>side3;
     s = ((side1 + side2 + side3)/2);
     area = sqrt(s*(s-side1)*(s-side2)*(s-side3));
-    cout<<"\n\tThe area of the triangle is : "<<area<<" unit square"<<endl;
+    cout<<"\n\tThe area of the triangle is : "<<area<<" unit square."<<endl;
 }     
