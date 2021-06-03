a4801880bd63a6e935b3482226fb6347953adb8f
Suman Basnet <Arunbasnet54@gmail.com>
2021-06-03 10:57:02 +0545

    Copy string function

---
+++ Lab_Works/Lab_3/Function_Built_in/cpystr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    char word1[25], word2[25];
+    cout<<"\tEnter any character : ";
+    
+    gets(word1);
+    
+    strcpy(word2,word1);
+    
+    cout<<"\n\tCopied string : ";
+    puts(word2);
+}
