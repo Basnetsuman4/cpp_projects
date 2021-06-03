f372ad9683fb81531112d3b90e99eca97b188113
Suman Basnet <Arunbasnet54@gmail.com>
2021-06-03 10:44:30 +0545

    Count string's length

---
+++ Lab_Works/Lab_3/Function_Built_in/lenstr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    char word[50];
+    int length;
+    cout<<"\tEnter any character : ";
+    
+    gets(word);
+    
+    length=strlen(word);
+    cout<<"\n\tThe length of the character is : "<<length<<".";
+    
+}
