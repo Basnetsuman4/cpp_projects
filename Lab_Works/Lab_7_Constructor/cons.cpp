23070f09937c38806ae133ac645a25bd972293f2
Suman Basnet <Arunbasnet54@gmail.com>
2021-06-18 13:40:46 +0545

    Constructor's illustration

---
+++ Lab_Works/Lab_7_Constructor/cons.cpp
@@ -0,0 +1,23 @@
+//Illustration of Constructor//
+#include<iostream>
+using namespace std;
+
+class Construct 
+{
+
+    public:
+        int  a, b;
+    Construct()
+    {
+
+
+        a = 10;
+        b = 20;
+    }
+};
+int main(){
+    Construct c;
+    cout<<"a: "<<c.a<<endl;
+    cout<<"b: "<<c.b;
+    return 1;
+}
